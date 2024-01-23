626a12f02b8fe098b7714ecd2454d555aff9dba8
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-23 17:47:13 +0530

    Database connection

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb+srv://abhinavchdhary:saymyname@cluster0.hyxgh5j.mongodb.net/?retryWrites=true&w=majority";
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
+
+module.exports = mongoDB;
