44856909226c6142858fcae019585bd4d4b3b116
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-23 16:20:43 +0530

    Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  return <div>App</div>;
+  const initialPostContent = { uniqueid: "", textcontent: "" };
+  const [postContent, setPostContent] = useState(initialPostContent);
+
+  const handleInputChange = (field, value) => {
+    setPostContent({
+      ...postContent,
+      [field]: value,
+    });
+  };
+
+  const handlePost = () => {
+    console.log("post", postContent);
+    // You can perform additional actions with the postContent state
+  };
+
+  const handleAnalyze = () => {
+    console.log("analyze", postContent.uniqueid);
+    // You can perform additional actions with the postContent state
+  };
+
+  return (
+    <div className="bg-dark text-light container p-4">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="uniqueid" className="form-label">
+            Unique Id:
+          </label>
+          <input
+            className="form-control"
+            type="number"
+            id="uniqueid"
+            value={postContent.uniqueid}
+            onChange={(e) => handleInputChange("uniqueid", e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="post-content" className="form-label">
+            Write your post:
+          </label>
+          <textarea
+            className="form-control"
+            id="post-content"
+            placeholder="Enter your post here..."
+            value={postContent.textcontent}
+            onChange={(e) => handleInputChange("textcontent", e.target.value)}
+          ></textarea>
+        </div>
+        <button
+          type="button"
+          className="btn btn-danger me-2"
+          onClick={handlePost}
+        >
+          Post
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleAnalyze}
+        >
+          Analyze
+        </button>
+      </form>
+    </div>
+  );
 }
