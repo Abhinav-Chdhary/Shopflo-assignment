7b06ac119fffe8d5c2f8f5da3e1ed97313c0408f
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-23 17:49:44 +0530

    DB connection & allow requests

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 
+//database connection
+const mongoDB = require("./db");
+mongoDB()
+  .then(() => {
+    console.log("Database operation completed successfully.");
+  })
+  .catch((error) => {
+    console.error("An error occurred during database operation:", error);
+  });
+
+//to allow requests from localhost:5173
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Include the allowed methods
+  next();
+});
+
 //middleware for using json data format
 app.use(express.json());
 
